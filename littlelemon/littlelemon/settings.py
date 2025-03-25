5d3b2596011833f190aaf0822d4f6718085b497b
sergio08301 <sergio08301@gmail.com>
2025-03-25 16:06:53 +0100

    Revisión final

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'Santiagosegura1',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
